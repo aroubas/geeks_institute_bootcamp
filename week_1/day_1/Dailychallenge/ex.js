70a29344f6c385e87a81cf96f9ceb87ff357de07
aroubas <calvinmahdi7@gmail.com>
2026-03-29 19:33:00 +0100

    "SOme xp"

--- week_1/day_1/Dailychallenge/ex.js
+++ week_1/day_1/Dailychallenge/ex.js
@@ -1,9 +1,12 @@
 let sentence = "The movie is not that bad, I like it";
+
 let wordNot = sentence.indexOf("not");
 let wordBad = sentence.indexOf("bad");
+
 if (wordBad > wordNot && wordNot !== -1 && wordBad !== -1) {
-  let part1 = sentence.slice(0, wordNot);
-        let part2 = sentence.slice(wordBad + 3);
-        sentence = part1 + "good" + part2;
+    let part1 = sentence.slice(0, wordNot);
+    let part2 = sentence.slice(wordBad + 3);
+    sentence = part1 + "good" + part2;
 }
+
 console.log(sentence);
